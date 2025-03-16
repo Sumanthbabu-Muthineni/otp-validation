3f74ff9bb2730cc9b19f3a23d458e65dc832e692
Sumanthbabu-Muthineni <sumanthbabu868@gmail.com>
2025-03-16 11:54:06 +0530

    added css for aesthetic experience

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import ThankYou from './components/ThankYou';
+import Welcome from './components/Welcome';
+import OtpVerification from './components/OtpVerification';
+import VerificationSent from './components/VerificationSent';
+import VerificationSuccess from './components/VerificationSuccess';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/thank-you" element={<ThankYou />} />
+                <Route path="/welcome" element={<Welcome />} />
+                <Route path="/otp" element={<OtpVerification />} />
+                <Route path="/verification-sent" element={<VerificationSent />} />
+                <Route path="/verification-success" element={<VerificationSuccess />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
